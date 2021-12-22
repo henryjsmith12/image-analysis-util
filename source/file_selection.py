1699b3e93c0a87efd614fe648ff64305121cc8f4
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-12-21 20:37:35 -0600

    Create file selection/creation classes

---
+++ source/file_selection.py
@@ -0,0 +1,21 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==============================================================================
+
+import pyqtgraph as pg
+from pyqtgraph import QtGui
+
+# ==============================================================================
+
+class FileSelectionWidget(QtGui.QWidget):
+    
+    def __init__(self):
+        super(FileSelectionWidget, self).__init__()
+
+# ==============================================================================
+
+class FileCreationWidget(QtGui.QWidget):
+    ...
