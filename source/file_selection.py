89bc74169159d58a4f21f945793b7154042ffcac
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-12-22 15:30:31 -0600

    Add full FileCreationWidget layout

--- source/file_selection.py
+++ source/file_selection.py
@@ -96,15 +96,62 @@ class FileCreationWidget(QtGui.QDialog):
     def __init__(self):
         super(FileCreationWidget, self).__init__()
 
+        # Class variables
+        self.file_path = ""
+        self.data_source_file_path = ""
+        self.dim_labels = "H, K, L"
+
+        # Window attributes
+        self.setWindowTitle("New File")
+
         # Subwidgets
-        self.select_data_source_txt = QtGui.QLineEdit()
-        self.select_data_source_txt.setReadOnly(True)
-        self.select_data_source_btn = QtGui.QPushButton("Select Data Source")
-        self.select_data_source_btn.setDefault(True)
+        self.source_type_lbl = QtGui.QLabel("Source Type: ")
+        self.source_type_cbx = QtGui.QComboBox()
+        self.source_type_cbx.addItems([".vti"])
+        self.data_source_lbl = QtGui.QLabel("Data Source: ")
+        self.data_source_txt = QtGui.QLineEdit()
+        self.data_source_txt.setReadOnly(True)
+        self.data_source_btn = QtGui.QPushButton("Browse")
+        self.dim_labels_lbl = QtGui.QLabel("Dimension Labels: ")
+        self.dim_labels_txt = QtGui.QLineEdit()
+        self.dim_labels_txt.setText(self.dim_labels)
+        self.save_as_lbl = QtGui.QLabel("Save File As: ")
+        self.save_as_txt = QtGui.QLineEdit()
+        self.save_as_txt.setReadOnly(True)
+        self.save_as_btn = QtGui.QPushButton("Browse")
+        self.cancel_btn = QtGui.QPushButton("Cancel")
+        self.create_file_btn = QtGui.QPushButton("Create File")
+        self.create_file_btn.setDefault(True)
+        self.create_file_btn.setEnabled(False)
 
         # Layout
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
+        self.layout.addWidget(self.source_type_lbl, 0, 0, 1, 2)
+        self.layout.addWidget(self.source_type_cbx, 0, 2, 1, 4)
+        self.layout.addWidget(self.data_source_lbl, 1, 0, 1, 2)
+        self.layout.addWidget(self.data_source_txt, 1, 2, 1, 2)
+        self.layout.addWidget(self.data_source_btn, 1, 4, 1, 2)
+        self.layout.addWidget(self.dim_labels_lbl, 2, 0, 1, 2)
+        self.layout.addWidget(self.dim_labels_txt, 2, 2, 1, 4)
+        self.layout.addWidget(self.save_as_lbl, 3, 0, 1, 2)
+        self.layout.addWidget(self.save_as_txt, 3, 2, 1, 2)
+        self.layout.addWidget(self.save_as_btn, 3, 4, 1, 2)
+        self.layout.addWidget(self.cancel_btn, 4, 0, 1, 2)
+        self.layout.addWidget(self.create_file_btn, 4, 4, 1, 2)
 
-        self.layout.addWidget(self.select_data_source_txt, 0, 0, 1, 2)
-        self.layout.addWidget(self.select_data_source_btn, 0, 2, 1, 2)
+        # Connections
+        self.data_source_btn.clicked.connect(self.SelectDataSource)
+    
+    # --------------------------------------------------------------------------
+
+    def SelectDataSource(self):
+        """
+        Gets data source file path and displays path in textbox.
+        """
+
+        self.data_source_file_path = QtGui.QFileDialog.getOpenFileName(self, \
+            "Select Data Source", "", f"(*{self.source_type_cbx.currentText})")[0]
+        self.data_source_txt.setText(self.data_source_file_path)
+
+    # --------------------------------------------------------------------------
