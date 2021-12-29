3c0f7849b88007f0f45ced7fffc3d77405c4d521
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-12-29 15:32:33 -0600

    Add commands to create roi/slicing windows

--- source/main_window.py
+++ source/main_window.py
@@ -12,8 +12,10 @@ import pyqtgraph as pg
 from pyqtgraph import QtGui, QtCore
 from pyqtgraph.dockarea import *
 
-from source.image_plot import ImagePlot
-from source.image_controller import ImageController
+from source.image_controller import *
+from source.image_plot import *
+from source.roi import *
+from source.slicing import *
 
 # ==============================================================================
 
@@ -48,6 +50,8 @@ class MainWindow(QtGui.QWidget):
 
         self.image_plot = ImagePlot(self)
         self.image_controller = ImageController(self)
+        self.roi_window = ROIWindow(self)
+        self.slicing_window = SlicingWindow(self)
 
     # --------------------------------------------------------------------------
 
@@ -58,11 +62,18 @@ class MainWindow(QtGui.QWidget):
 
         self.image_plot_dock = Dock("ImagePlot", size=(100, 100), hideTitle=True)
         self.image_controller_dock = Dock("ImageController", size=(100, 100), hideTitle=True)
+        self.roi_window_dock = Dock("ROI", size=(100, 200))
+        self.slicing_window_dock = Dock("Slicing", size=(100, 200))
 
         # Adds widgets to docks
         self.image_plot_dock.addWidget(self.image_plot)
         self.image_controller_dock.addWidget(self.image_controller)
+        self.roi_window_dock.addWidget(self.roi_window)
+        self.slicing_window_dock.addWidget(self.slicing_window)
 
         # Adds docks to dock area
         self.dock_area.addDock(self.image_plot_dock)
+        self.dock_area.addDock(self.slicing_window_dock, "right", self.image_plot_dock)
+        self.dock_area.addDock(self.roi_window_dock, "above", self.slicing_window_dock)
         self.dock_area.addDock(self.image_controller_dock, "bottom", self.image_plot_dock)
+        
