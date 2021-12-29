dbd9cb07cffc6da51c7649f6c3896f27577d8996
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-12-29 15:13:56 -0600

    Add dock area + docks

--- source/main_window.py
+++ source/main_window.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph import QtGui, QtCore
+from pyqtgraph.dockarea import *
 
 from source.image_plot import ImagePlot
 from source.image_controller import ImageController
@@ -24,13 +25,44 @@ class MainWindow(QtGui.QWidget):
     def __init__(self, file_path) -> None:
         super().__init__()
 
+        # Class Variables 
         self.file_path = file_path
+
+        # Subwidgets
+        self.dock_area = DockArea()
+
+        # Layout
+        self.layout = QtGui.QVBoxLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(self.dock_area)
+
+        self.createWidgets()
+        self.createWidgetDocks()
+
+    # --------------------------------------------------------------------------
+
+    def createWidgets(self):
+        """
+        
+        """
 
         self.image_plot = ImagePlot(self)
         self.image_controller = ImageController(self)
 
-        self.layout = QtGui.QGridLayout()
-        self.setLayout(self.layout)
+    # --------------------------------------------------------------------------
 
-        self.layout.addWidget(self.image_plot, 0, 0)
-        self.layout.addWidget(self.image_controller, 1, 0)
+    def createWidgetDocks(self):
+        """
+        
+        """
+
+        self.image_plot_dock = Dock("ImagePlot", size=(100, 100), hideTitle=True)
+        self.image_controller_dock = Dock("ImageController", size=(100, 100), hideTitle=True)
+
+        # Adds widgets to docks
+        self.image_plot_dock.addWidget(self.image_plot)
+        self.image_controller_dock.addWidget(self.image_controller)
+
+        # Adds docks to dock area
+        self.dock_area.addDock(self.image_plot_dock)
+        self.dock_area.addDock(self.image_controller_dock, "bottom", self.image_plot_dock)
