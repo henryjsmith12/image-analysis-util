0008881fc8613526f2708688445970b1d100bd98
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-12-29 08:55:14 -0600

    Rename classes

--- source/main_window.py
+++ source/main_window.py
@@ -11,13 +11,14 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph import QtGui, QtCore
 
-from source.image_widget import ImageWidget
+from source.image_plot import ImagePlot
+from source.image_controller import ImageController
 
 # ==============================================================================
 
 class MainWindow(QtGui.QWidget):
     """
-    Creates/places widgets for analysis window
+    Houses widgets for analysis window
     """
 
     def __init__(self, file_path) -> None:
@@ -25,9 +26,11 @@ class MainWindow(QtGui.QWidget):
 
         self.file_path = file_path
 
-        self.image_widget = ImageWidget(self)
+        self.image_plot = ImagePlot(self)
+        self.image_controller = ImageController(self)
 
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
 
-        self.layout.addWidget(self.image_widget, 0, 0)
+        self.layout.addWidget(self.image_plot, 0, 0)
+        self.layout.addWidget(self.image_controller, 1, 0)
