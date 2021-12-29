30d216282650c843979ff7bd76350c3a221b111c
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-12-29 15:31:59 -0600

    Rename file

---
+++ source/roi.py
@@ -0,0 +1,26 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==============================================================================
+
+import h5py
+import os
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph import QtGui, QtCore
+
+# ==============================================================================
+
+class ROIWindow(QtGui.QWidget):
+    """
+    Houses ROI widgets.
+    """
+
+    def __init__(self, parent) -> None:
+        super(ROIWindow, self).__init__(parent)
+        self.main_window = parent
+
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
