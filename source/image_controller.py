9b0e3c45a23f1d5685776c1bf3a4e67c1276d932
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-12-29 16:05:34 -0600

    Add sliders

--- source/image_controller.py
+++ source/image_controller.py
@@ -22,5 +22,20 @@ class ImageController(QtGui.QWidget):
         super(ImageController, self).__init__(parent)
         self.main_window = parent
 
+        # Subwidgets
+        self.slider1 = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider1_lbl = QtGui.QLabel()
+        self.slider1_cbx = QtGui.QComboBox()
+        self.slider2 = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider2_lbl = QtGui.QLabel()
+        self.slider2_cbx = QtGui.QComboBox()
+        
+        # Layout
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
+        self.layout.addWidget(self.slider1_lbl, 0, 0)
+        self.layout.addWidget(self.slider1, 0, 1, 1, 6)
+        self.layout.addWidget(self.slider1_cbx, 0, 7)
+        self.layout.addWidget(self.slider2_lbl, 1, 0)
+        self.layout.addWidget(self.slider2, 1, 1, 1, 6)
+        self.layout.addWidget(self.slider2_cbx, 1, 7)
