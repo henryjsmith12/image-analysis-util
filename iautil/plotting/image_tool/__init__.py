ea106327e9e411f10499eb45501ae0b4dc4d1b65
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-01-11 22:16:43 -0600

    Add placeholder for ImageTool class

---
+++ iautil/plotting/image_tool/__init__.py
@@ -0,0 +1,28 @@
+"""
+A general tool for plotting, slicing, and analyzing xarray DataArrays.
+"""
+
+# ----------------------------------------------------------------------------------
+
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph import QtGui, QtCore
+
+# ----------------------------------------------------------------------------------
+
+__all__ = (
+    "ImageTool",
+    "image_tool"
+)
+
+# ----------------------------------------------------------------------------------
+
+class ImageTool(QtGui.QApplication):
+    """
+    
+    """
+    
+    def __init__(self, data_array) -> None:
+        super(ImageTool).__init__()
+
+        self.data_array = data_array
