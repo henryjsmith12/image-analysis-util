26e6692fae8c0da042433908e5616343222d8a5c
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-01-12 10:02:29 -0600

    Add dock area for ImageTool

--- iautil/plotting/image_tool/__init__.py
+++ iautil/plotting/image_tool/__init__.py
@@ -6,35 +6,92 @@ A general tool for plotting, slicing, and analyzing xarray DataArrays.
 
 import numpy as np
 import pyqtgraph as pg
-from pyqtgraph import QtGui, QtCore
+from pyqtgraph import dockarea, QtGui, QtCore
+import xarray as xr
 
 # ----------------------------------------------------------------------------------
 
 __all__ = (
     "ImageTool",
-    "image_tool"
+    "ImageToolWidget"
 )
 
 # ----------------------------------------------------------------------------------
 
-class ImageTool(QtGui.QApplication):
+class ImageTool:
     """
     
     """
     
-    def __init__(self, data_array) -> None:
-        super(ImageTool).__init__()
-
+    def __init__(self, data_array: xr.DataArray) -> None:  
         self.data_array = data_array
+
+        self.app = pg.mkQApp("Image Analysis")
+        self.image_tool_widget = ImageToolWidget(data_array)
+        self.image_tool_widget.show()
+        self.app.exec_()
 
 # ----------------------------------------------------------------------------------
 
 class ImageToolWidget(QtGui.QWidget):
-    ...
+    """
+
+    """
+
+    def __init__(self, data_array: xr.DataArray) -> None:
+        super(ImageToolWidget, self).__init__()
+
+        self.data_array = data_array
+
+        self.data_array_plot = None
+        self.controller_widget = None
+
+        self.dock_area = dockarea.DockArea()
+        self.data_array_plot_dock = None
+        self.controller_widget_dock = None
+        
+        self._create_widgets()
+        self._create_docks()
+
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(self.dock_area)
+        self.setLayout(self.layout)
+
+    # ------------------------------------------------------------------------------
+
+    def _create_widgets(self) -> None:
+        """
+        
+        """
+        
+        # Generic placeholder widgets
+        self.data_array_image_view = pg.ImageView()
+        self.controller_widget = QtGui.QWidget()
+
+    # ------------------------------------------------------------------------------
+    
+    def _create_docks(self) -> None:
+        """
+        
+        """
+
+        self.data_array_image_view_dock = dockarea.Dock(
+            name="Data Array Plot",
+            size=(200, 200),
+            widget=self.data_array_image_view,
+            hideTitle=True
+        )
+
+        self.controller_widget_dock = dockarea.Dock(
+            name="Controller",
+            size=(200, 100),
+            widget=self.controller_widget,
+            hideTitle=True
+        )
+
+        self.dock_area.addDock(self.data_array_image_view_dock)
+        self.dock_area.addDock(self.controller_widget_dock)
 
 # ----------------------------------------------------------------------------------
 
-class ImageToolLayout(QtGui.QGridLayout):
-    ...
-
-# ----------------------------------------------------------------------------------
+ImageTool(xr.DataArray(np.array([1,2,3])))
