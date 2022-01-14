057a3f562c385169def54adea3554be758d90938
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-01-14 10:07:01 -0600

    Placeholder for DataArrayController class

--- iautil/plotting/image_tool/DataArrayController.py
+++ iautil/plotting/image_tool/DataArrayController.py
@@ -4,7 +4,24 @@ Widget that controls the DataArray loaded into an ImageTool instance.
 
 # ----------------------------------------------------------------------------------
 
-
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph import QtGui, QtCore
+import xarray as xr
 
 # ----------------------------------------------------------------------------------
+
+__all__ = (
+    "DataArrayController"
+)
+
+# ----------------------------------------------------------------------------------
+
+class DataArrayController(QtGui.QWidget):
+    """
+    
+    """
+
+    def __init__(self) -> None:
+        super(DataArrayController).__init__()
 
