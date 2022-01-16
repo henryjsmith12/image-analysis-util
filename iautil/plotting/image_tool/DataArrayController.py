259a9a92c83fb5e068d48a19d63439143eed5706
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-01-15 19:49:43 -0500

    Add function to update current value of combobox

--- iautil/plotting/image_tool/DataArrayController.py
+++ iautil/plotting/image_tool/DataArrayController.py
@@ -6,7 +6,7 @@ Widget that controls the DataArray loaded into an ImageTool instance.
 
 import numpy as np
 import pyqtgraph as pg
-from pyqtgraph import QtGui, QtCore
+from PyQt5 import QtGui, QtCore
 import xarray as xr
 
 # ----------------------------------------------------------------------------------
@@ -33,6 +33,9 @@ class DataArrayController(QtGui.QWidget):
     def __init__(self, data_array: xr.DataArray) -> None:
         super(DataArrayController, self).__init__()
 
+        self.data_array = data_array
+        self.updated = False
+
         self.layout = DataArrayControllerLayout(data_array, parent=self)
         self.setLayout(self.layout)
 
@@ -42,6 +45,29 @@ class DataArrayController(QtGui.QWidget):
         self.dim_currval_cbx_list = self.layout.dim_currval_cbx_list
 
     # ------------------------------------------------------------------------------
+
+    def _update_currval(self) -> None:
+        """
+        
+        """
+        
+        if isinstance(self.sender(), QtGui.QSlider) and not self.updated:
+            dim = self.dim_slider_list.index(self.sender())
+            index = self.sender().value()
+            self.dim_currval_cbx_list[dim].setCurrentIndex(index)
+            #self.updated = True
+        if isinstance(self.sender(), QtGui.QComboBox) and not self.updated:
+            dim = self.dim_currval_cbx_list.index(self.sender())
+            index = self.sender().currentIndex()
+            self.dim_slider_list[dim].setValue(index)
+            #self.updated = True
+
+    # ------------------------------------------------------------------------------
+
+    def _update_location(self) -> None:
+        """
+        
+        """
 
 # ----------------------------------------------------------------------------------
 
@@ -69,15 +95,20 @@ class DataArrayControllerLayout(QtGui.QGridLayout):
 
             self.dim_location_cbx_list[i].addItems(DIMENSION_LOCATIONS)
             self.dim_location_cbx_list[i].setCurrentIndex(i)
-
             self.dim_slider_list[i].setMaximum(data_array.shape[i] - 1)
-
             self.dim_currval_cbx_list[i].addItems(dim_coords)
 
             self.addWidget(self.dim_lbl_list[i], i, 0)
             self.addWidget(self.dim_location_cbx_list[i], i, 1)
             self.addWidget(self.dim_slider_list[i], i, 2, 1, 3)
             self.addWidget(self.dim_currval_cbx_list[i], i, 5)
+
+            self.dim_slider_list[i].valueChanged.connect(
+                parent._update_currval
+            )
+            self.dim_currval_cbx_list[i].valueChanged.connect(
+                parent._update_currval
+            )
 
 # ----------------------------------------------------------------------------------
 
