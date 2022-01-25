6b1db950c7befeb74f7c2dc38acfb7b552709dd8
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-01-25 17:15:24 -0600

    Add placeholders for widget and layout classes

---
+++ iautil/plotting/image_tool/slicing.py
@@ -0,0 +1,23 @@
+"""
+Displays views from arbitrary slices of a DataArray
+"""
+
+# ----------------------------------------------------------------------------------
+
+import numpy as np
+import pyqtgraph as pg
+from PyQt5 import QtGui, QtCore
+import xarray as xr
+
+# ----------------------------------------------------------------------------------
+
+class SlicingWidget(QtGui.QWidget):
+    ...
+
+# ----------------------------------------------------------------------------------
+
+class SlicingWidgetLayout(QtGui.QGridLayout):
+    ...
+    
+# ----------------------------------------------------------------------------------
+
