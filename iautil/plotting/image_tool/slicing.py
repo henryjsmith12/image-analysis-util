c48aa6e7506101e1a26c595a4186f82dd49ff0dc
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-01-25 17:54:04 -0600

    Add workflow plans for layout

--- iautil/plotting/image_tool/slicing.py
+++ iautil/plotting/image_tool/slicing.py
@@ -12,12 +12,34 @@ import xarray as xr
 # ----------------------------------------------------------------------------------
 
 class SlicingWidget(QtGui.QWidget):
-    ...
+    """
+    Allows user to view arbitrary slices/linecuts of a DataArray
+    """
+    
+    def __init__(self, data_array: xr.DataArray, parent=None) -> None:
+        super(SlicingWidget, self).__init__(parent)
+
+        self.parent = parent
+        self.data_array = data_array
+        
+        # Custom layout
+        self.layout = SlicingWidgetLayout(data_array, parent=self)
+        self.setLayout(self.layout)
 
 # ----------------------------------------------------------------------------------
 
 class SlicingWidgetLayout(QtGui.QGridLayout):
-    ...
+    """
+    Custom dynamic grid layout for slicing widget.
+
+    WORKFLOW:
+    2D: LineROI in ImageView -> 1D Plot
+    3D: LineROI in ImageView -> 2D ImageView -> 1D Plot
+    4D: LineROI in ImageView -> 3D ImageView w/ slider -> 2D ImageView -> 1D Plot
+    """
     
+    def __init__(self, data_array: xr.DataArray, parent=None) -> None:
+        super(SlicingWidgetLayout, self).__init__(parent)
+
 # ----------------------------------------------------------------------------------
 
