adb2a7f72488d29c3d8344dd5a5d6bbebdd87019
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-02-16 21:57:01 -0600

    Reworked io functions, added extra error handling

--- iautil/io.py
+++ iautil/io.py
@@ -1,6 +1,8 @@
 """
-Basic I/O functions for creating/loading .iau files and reading from 
-various file types.
+Basic I/O functions for:
+- Creating IAU file from manual arguments
+- Creating IAU file from a .vti (VTK Image Data) file/directory
+- Creating an xarray DataArray from a IAU file
 """
 
 # ----------------------------------------------------------------------------------
@@ -17,85 +19,114 @@ import xarray as xr
 # ----------------------------------------------------------------------------------
 
 __all__ = (
-    "create_iau_file",
-    "load_iau_file"
+    "create_iau",
+    "vti_to_iau",
+    "iau_to_data_array"
 )
 
 # ----------------------------------------------------------------------------------
 
-def create_iau_file (
-    data_source: str, 
-    iau_file_path: str, 
-    axis_labels: List[str] = None, 
-    new_axis_values: List = None,
+def create_iau(
+    iau_path: str,
+    data: np.ndarray,
+    coords: list = None,
+    dims: list = None,
     metadata: dict = None
-) -> None:
+):
     """
-    Creates .iau file from data source. Data source can either be a single file
-    or a directory of multiple data source files.
+    Creates IAU file.
 
     Parameters:
-    data_source (str): File/directory path used to create .iau file from.
-    iau_file_path (str): File path to save .iau file in.
-    axis_labels (list[str]): Labels for each axis.
-    new_axis_values (list): For .iau files created from multiple data source files
-        stitched together. Defines values for new axis.
-    metadata (dict): Metadata for .iau file.
+    iau_path (str): Path to save file in.
+    data (np.ndarray): A numpy array of data points to be stored in file.
+    coords (list): A list of lists of coordinates for each dimension of dataset.
+    dims (list): A list of labels for each dimension of dataset.
+    metadata (dict): Metadata for file.
     """
 
-    # Data source validation
-    if not os.path.exists(data_source):
-        raise OSError(
-            f"{data_source} is an invalid path."
-        )
+    if iau_path is None:
+        raise ValueError("IAU path not given.")
+    if type(iau_path) != str:
+        raise ValueError("IAU path must be a string.")
 
-    # Load data and axes from data source
-    data, axes = None, None
-    
-    # Data source as directory
-    if os.path.isdir(data_source):
-        file_list = os.listdir(data_source) # directory contents, sorted
-        file_list.sort()
-        data_list, axes_list = [], []
+    if data is None:
+        raise ValueError("data not given.")
+    if type(data) != np.ndarray:
+        raise ValueError("data must be a numpy ndarray.")
 
-        for file in file_list:
-            data, axes = _load_data_source(str(os.path.join(data_source, file)))
-            data_list.append(data)
-            axes_list.append(axes)
-        data, axes = _stitch(data_list, axes_list)
+    if coords is not None:
+        if type(coords) != list:
+            raise ValueError("coords must be a list.")
 
-        # Handles new axis values 
-        if new_axis_values is None:
-            new_axis_values = [i for i in range(data.shape[-1])]
-        axes.append(new_axis_values)
-    # Data source as file
-    elif os.path.isfile(data_source):
-        data, axes = _load_data_source(data_source)
+    if dims is not None:
+        if type(dims) != list:
+            raise ValueError("dims must be a list.")
 
-    # Handles axis labels
-    if axis_labels is None:
-        axis_labels = [f"axis_{i}" for i in range(data.ndim)]
-    elif len(axis_labels) != data.ndim:
-        raise ValueError(
-            f"Invalid number of axis labels. Expected # of labels: {data.ndim}"
-        )
+    if metadata is not None:
+        if type(metadata) != dict:
+            raise ValueError("metadata must be a dictionary.")
 
-    # Creates .iau file
-    with h5py.File(iau_file_path, 'a') as new_file:
+    if None not in [coords, dims] and len(coords) != len(dims):
+        raise RuntimeError("Dimension sizes for coords and dims do not match.")
+
+    with h5py.File(iau_path, "a") as new_file:
         new_file.create_dataset("data", data=data)
         new_file.attrs["metadata"] = str(metadata)
         new_file.create_group("axes")
 
-        for i in range(len(axes)):
-            axis = np.array(axes[i])
-            new_file.create_dataset(f"axes/axis_{i}", data=axis)
-            new_file["data"].dims[i].label = axis_labels[i]
-            new_file[f"axes/axis_{i}"].make_scale(axis_labels[i])
-            new_file["data"].dims[i].attach_scale(new_file[f"axes/axis_{i}"])
+        if dims is None:
+            dims = [f"dim_{i}" for i in range(data.ndim)]
+
+        for i in range(len(coords)):
+            new_file["data"].dims[i].label = dims[i]
+
+            if coords is not None:
+                axis = np.array(coords[i])
+                new_file.create_dataset(f"axes/axis_{i}", data=axis)
+                new_file[f"axes/axis_{i}"].make_scale(dims[i])
+                new_file["data"].dims[i].attach_scale(new_file[f"axes/axis_{i}"])
 
 # ----------------------------------------------------------------------------------
 
-def load_iau_file(file: str) -> xr.DataArray:
+def vti_to_iau(
+    vti_path: str,
+    iau_path: str,
+    new_dim_coords: list = None,
+    dims: list = None,
+    metadata: dict = None
+):
+    # Data source as directory
+    if os.path.isdir(vti_path):
+        file_list = os.listdir(vti_path) # directory contents, sorted
+        file_list.sort()
+        data_list, coords_list = [], []
+
+        for file in file_list:
+            data, coords = _load_vti(str(os.path.join(vti_path, file)))
+            data_list.append(data)
+            coords_list.append(coords)
+        data, coords = _stitch(data_list, coords_list)
+
+        # Handles new axis values 
+        if new_dim_coords is None:
+            new_dim_coords = [i for i in range(data.shape[-1])]
+        coords.append(new_dim_coords)
+
+    # Data source as file
+    else:
+        data, coords = _load_vti(vti_path)
+
+    create_iau(
+        iau_path=iau_path,
+        data=data,
+        coords=coords,
+        dims=dims,
+        metadata=metadata
+    )
+
+# ----------------------------------------------------------------------------------
+
+def iau_to_data_array(iau_path: str):
     """
     Retrieves data, axis info, and metadata from .iau file in an xarray dataset.
 
@@ -107,98 +138,29 @@ def load_iau_file(file: str) -> xr.DataArray:
     """
     
     # Reads info from .iau file
-    with h5py.File(file, 'r') as iau_file:
+    with h5py.File(iau_path, 'r') as iau_file:
         data = iau_file["data"][...]
-        axes = [iau_file["data"].dims[i][0][...] for i in range(data.ndim)]
-        axis_labels = [iau_file["data"].dims[i].label for i in range(data.ndim)]
+        coords = [iau_file["data"].dims[i][0][...] for i in range(data.ndim)]
+        dims = [iau_file["data"].dims[i].label for i in range(data.ndim)]
         metadata = ast.literal_eval(iau_file.attrs["metadata"])
 
     # Creates xarray DataArray from .iau info
     # Internal data structure for everything in image-analysis-util
     data_array = xr.DataArray(
         data=data, 
-        coords=axes, 
-        dims=axis_labels, 
+        coords=coords, 
+        dims=dims, 
         attrs=metadata
     )
 
     return data_array
-    
-# ----------------------------------------------------------------------------------
-
-def _load_data_source(file: str) -> Tuple[np.ndarray, List[list]]:
-    """
-    Retrieves data and axis values from data source file.
-
-    Parameters:
-        file (str): Data source file to load.
-
-    Returns:
-        data (np.ndarray): Multi-dimensional NumPy array holding data.
-        axes (list[list]): List of values for each axis.
-    """
-    
-    file_ext = os.path.splitext(file)[1]
-    data, axes = None, None
-
-    if file_ext == ".vti":
-        data, axes = _load_vti(file)
-
-    return data, axes
 
 # ----------------------------------------------------------------------------------
 
-def _stitch(
-    data_list: List[np.ndarray],
-    axes_list: List[list]
-) -> Tuple[np.ndarray, List[list]]:
-    """
-    _stitches data from multiple data source files. Also checks for inconsistencies 
-    in axis values.
+def _load_vti(vti_path: str):
 
-    Parameters:
-        data_list (list[np.ndarray]): List of data from each data source file.
-        axes_list (list[list]): List of axis values from each data source file.
-
-    Returns:
-        data (np.ndarray): Stacked data arrays; n + 1 dimensions.
-        axes (list[list]): Axis values from each axis.
-    """
-    
-    data, axes = None, None
-
-    # Checks if axes stay consistent throughout data source files
-    consistent_axes = axes_list.count(axes_list[0]) == len(axes_list)
-
-    if consistent_axes:
-        axes = axes_list[0]
-    else:
-        raise ValueError(
-            "Inconsistent axes throughout data source files."
-        )
-
-    # Converts list of NumPy arrays (ndim = n) to one NumPy array (ndim = n + 1)
-    data = np.stack(data_list, axis=-1)
-    
-    return data, axes
-
-# ----------------------------------------------------------------------------------
-
-def _load_vti(file: str) -> Tuple[np.ndarray, List[list]]:
-    """
-    Retrieves data, axis values from .vti (VTK XML image format) file.
-    .vti files contain 2D or 3D datasets.
-
-    Parameters:
-        file (str): .vti file to load.
-
-    Returns:
-        data (np.ndarray): Multi-dimensional NumPy array holding data.
-        axes (list[list]): List of values for each axis.
-    """
-    
     data_reader = vtk.vtkXMLImageDataReader()
-    data_reader.SetFileName(file)
+    data_reader.SetFileName(vti_path)
     data_reader.Update()
 
     raw_data = data_reader.GetOutput()
@@ -213,7 +175,6 @@ def _load_vti(file: str) -> Tuple[np.ndarray, List[list]]:
 
     axis_0, axis_1, axis_2 = [], [], []
 
-    # Adds values to each axis accordingly
     for point in range(extent[0], extent[1] + 1):
         axis_0.append(origin[0] + point * spacing[0])
     for point in range(extent[2], extent[3] + 1):
@@ -222,8 +183,31 @@ def _load_vti(file: str) -> Tuple[np.ndarray, List[list]]:
         axis_2.append(origin[2] + point * spacing[2])
 
     # A list of lists of varying lengths
-    axes = [axis_0, axis_1, axis_2]
+    coords = [axis_0, axis_1, axis_2]
 
-    return data, axes
+    return data, coords
+
+# ----------------------------------------------------------------------------------
+
+def _stitch(
+    data_list: List[np.ndarray],
+    coords_list: List[list]
+):
+    data, coords = None, None
+
+    # Checks if coords stay consistent throughout data source files
+    consistent_coords = coords_list.count(coords_list[0]) == len(coords_list)
+
+    if consistent_coords:
+        coords = coords_list[0]
+    else:
+        raise ValueError(
+            "Inconsistent coords throughout data source files."
+        )
+
+    # Converts list of NumPy arrays (ndim = n) to one NumPy array (ndim = n + 1)
+    data = np.stack(data_list, axis=-1)
+    
+    return data, coords
 
 # ----------------------------------------------------------------------------------
