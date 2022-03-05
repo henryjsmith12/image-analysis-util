6e5e0cdd5cdd6400f4df3dc07285fb8079e7361e
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-03-05 11:26:38 -0600

    Add test file for 4D ImageTool

---
+++ tests/test_4d_image_tool.py
@@ -0,0 +1,19 @@
+from iautil import io
+from iautil.plotting.image_tool import ImageTool
+import os
+
+
+scans_vti_path = os.path.abspath("tests/test_files")
+scans_iau_path = os.path.abspath("tests/test_files/scans40-42.iau")
+
+io.vti_to_iau(
+    vti_path=scans_vti_path,
+    iau_path=scans_iau_path,
+    dims=["H", "K", "L", "V"]
+)
+
+da = io.iau_to_data_array(scans_iau_path)
+os.remove(scans_iau_path)
+
+it = ImageTool(da)
+it.show()
