79703b3523bee4e08012cf22be154d00bde6dbf7
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-03-05 11:26:25 -0600

    Add test file for 3D ImageTool

---
+++ tests/test_3d_image_tool.py
@@ -0,0 +1,19 @@
+from iautil import io
+from iautil.plotting.image_tool import ImageTool
+import os
+
+
+scan40_vti_path = os.path.abspath("tests/test_files/scan40.vti")
+scan40_iau_path = os.path.abspath("tests/test_files/scan40.iau")
+
+io.vti_to_iau(
+    vti_path=scan40_vti_path,
+    iau_path=scan40_iau_path,
+    dims=["H", "K", "L"]
+)
+
+da = io.iau_to_data_array(scan40_iau_path)
+os.remove(scan40_iau_path)
+
+it = ImageTool(da)
+it.show()
