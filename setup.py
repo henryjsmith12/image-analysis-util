5ae03c7a8893d5349ab4c729e357e66cff1e9cef
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-03-31 20:12:52 -0500

    Change for v0.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (this_dir / "README.md").read_text()
 
 setup( 
     name="image-analysis-util",
-    version="0.1.2",
+    version="0.1.3",
     description="Scientific image exploration software for multidimensional datasets.",
     long_description=long_description,
     long_description_content_type="text/markdown",
