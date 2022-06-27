46703f1e1b95ab3bf74510921126ea37201c0b0b
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-06-27 17:48:06 -0500

    Basic ROI tab layout and centering functionality

--- iautil/plotting/image_tool/roi.py
+++ iautil/plotting/image_tool/roi.py
@@ -1,5 +1,5 @@
 """
-Displays views from arbitrary slices of a DataArray
+Displays  of a DataArray
 """
 
 # ----------------------------------------------------------------------------------
@@ -48,29 +48,25 @@ class ROIWidget(dockarea.DockArea):
         # Parent tab and DataArrayController from overall ImageTool
         self.tab = tab
         self.main_controller = tab.parent.data_array_controller
+        self.main_image_view = tab.parent.data_array_image_view
         
         self.data_array = data_array
 
-        # Creates SlicingROI
-        self.roi = ROI()
-        self.roi.hide()
-        self.roi.roi_widget = self
-        self.roi.parent_imv = tab.parent.data_array_image_view
-        self.roi.parent_imv.addItem(self.roi)
-
-        self.controller = ROIController(self, self.data_array)
-        self.roi_image_view = ROIImageView(self)
+        self.roi = ROI(parent=self, image_view=self.main_image_view)
+        self.roi.removeHandle(0)
+        self.controller = ROIController(parent=self, data_array=self.data_array)
+        self.roi_image_view = ROIImageView(parent=self, data_array=self.data_array)
 
         # Docks
         self.controller_dock = dockarea.Dock(
             name="Controller",
-            size=(200, 100),
+            size=(100, 20),
             widget=self.controller,
             hideTitle=True
         )
         self.roi_image_view_dock = dockarea.Dock(
             name="Image View",
-            size=(200, 100),
+            size=(100, 200),
             widget=self.roi_image_view,
             hideTitle=True
         )
@@ -83,30 +79,157 @@ class ROIWidget(dockarea.DockArea):
 
 class ROI(pg.RectROI):
     
-    def __init__(self, position=(0,0), size=(1,1), parent=None) -> None:
+    def __init__(self, position=(0,0), size=(1,1), parent=None, image_view=None) -> None:
         super(ROI, self).__init__(position, size)
         
-        self.roi_widget = None
+        self.parent = parent
+        self.image_view = image_view
+
+        self.addTranslateHandle(pos=(0.5, 0.5))
+        self.addScaleHandle(pos=(0, 0.5), center=(0.5, 0.5))
+        self.addScaleHandle(pos=(1, 0.5), center=(0.5, 0.5))
+        self.addScaleHandle(pos=(0.5, 0), center=(0.5, 0.5))
+        self.addScaleHandle(pos=(0.5, 1), center=(0.5, 0.5))
+        
+        self.hide()
+        if type(self.image_view) == DataArrayImageView:
+            self.image_view.addItem(self)
+
+        self.sigRegionChanged.connect(self.get_data_array_region)
+
+    # ------------------------------------------------------------------------------
+
+    def get_data_array_region(self):
+
+        data_array = self.image_view.data_array
+        data_array_slice = self.image_view.data_array_slice
+
+        data, coords = self.getArrayRegion(
+            data=data_array_slice,
+            img=self.image_view.getImageItem(),
+            returnMappedCoords=True
+        )
+        print(coords[0][25][25])
+
+        x_coords, y_coords = coords
+
+        x_1 = x_coords[0][0]
+        x_2 = x_coords[-1][-1]
+        y_1 = y_coords[0][0]
+        y_2 = y_coords[-1][-1]
+
+        h = self.image_view.getImageItem().mapFromData(QtCore.QPoint(0,0)) 
+
+        pg.ImageItem.mapFromData
+        print(h)
+        avgs = []
+        '''for i in range(data_array.shape[2]):
+            for j in range(data_array.shape[3]):
+                avg = data_array[x_1:x_2, y_1:y_2, i, j].shape
+                #print(avg)'''
+
+        #data_array_slice = data_array[x_1:x_2,y_1:y_2]
+
+        '''self.parent.roi_image_view.set_data_array_slice(
+            data_array,
+            data_array_slice
+        )'''
+
+    # ------------------------------------------------------------------------------
+
+    def center(self):
+        """
+        Centers ROI.
+        """
+        data_array = self.image_view.data_array
+        x_1 = data_array.coords[data_array.dims[0]].values[0]
+        x_2 = data_array.coords[data_array.dims[0]].values[-1]
+        y_1 = data_array.coords[data_array.dims[1]].values[0]
+        y_2 = data_array.coords[data_array.dims[1]].values[-1]
+
+        self.movePoint(self.getHandles()[0], ((x_1 + x_2)/2, (y_1+y_2)/2))
+        self.movePoint(self.getHandles()[1], (x_1, (y_1+y_2)/2))
+        self.movePoint(self.getHandles()[2], (x_2, (y_1+y_2)/2))
+        self.movePoint(self.getHandles()[3], ((x_1 + x_2)/2, y_1))
+        self.movePoint(self.getHandles()[4], ((x_1 + x_2)/2, y_2))
 
 # ----------------------------------------------------------------------------------
 
 class ROIController(QtGui.QWidget):
 
-    def __init__(self, roi_widget, data_array) -> None:
-            super(ROIController, self).__init__(roi_widget)
+    def __init__(self, parent=None, data_array=None) -> None:
+        super(ROIController, self).__init__()
+
+        self.parent = parent
+        self.data_array = data_array
+
+        self.width_lbl = QtGui.QLabel("Width")
+        self.center_px_lbl = QtGui.QLabel("Center")
+        self.dim_1_lbl = QtGui.QLabel()
+        self.dim_1_width_sbx = QtGui.QDoubleSpinBox()
+        self.dim_1_center_cbx = QtGui.QComboBox()
+        self.dim_2_lbl = QtGui.QLabel()
+        self.dim_2_width_sbx = QtGui.QDoubleSpinBox()
+        self.dim_2_center_cbx = QtGui.QComboBox()
+        self.enable_chkbx = QtGui.QCheckBox("Enable")
+        self.center_btn = QtGui.QPushButton("Center ROI")
+
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(self.width_lbl, 0, 1)
+        self.layout.addWidget(self.center_px_lbl, 0, 2)
+        self.layout.addWidget(self.dim_1_lbl, 1, 0)
+        self.layout.addWidget(self.dim_1_width_sbx, 1, 1)
+        self.layout.addWidget(self.dim_1_center_cbx, 1, 2)
+        self.layout.addWidget(self.dim_2_lbl, 2, 0)
+        self.layout.addWidget(self.dim_2_width_sbx, 2, 1)
+        self.layout.addWidget(self.dim_2_center_cbx, 2, 2)
+        self.layout.addWidget(self.enable_chkbx, 3, 0)
+        self.layout.addWidget(self.center_btn, 3, 1)
+        self.layout.setRowStretch(0, 1)
+        self.layout.setRowStretch(1, 2)
+        self.layout.setRowStretch(2, 2)
+        self.layout.setRowStretch(3, 2)
+
+        self.enable_chkbx.stateChanged.connect(self.toggle_enabled)
+        self.center_btn.clicked.connect(self.parent.roi.center)
+
+    # ------------------------------------------------------------------------------
+
+    def toggle_enabled(self):
+        if self.enable_chkbx.isChecked():
+            self.enable()
+        else:
+            self.disable()
+
+    # ------------------------------------------------------------------------------
+
+    def enable(self):
+        self.enabled = True
+
+        self.parent.roi.show()
+        self.parent.roi.center()
+
+    # ------------------------------------------------------------------------------
+
+    def disable(self):
+        self.enabled = False
+
+        self.parent.roi_image_view.clear()
+        self.parent.roi.hide()
 
 # ----------------------------------------------------------------------------------
 
 class ROIImageView(DataArrayImageView):
-    
-    def __init__(self, parent=None) -> None:
-        super(ROIImageView, self).__init__(
-            parent
-        )
+
+    def __init__(self, parent=None, data_array=None) -> None:
+        super(ROIImageView, self).__init__(parent)
 
 # ----------------------------------------------------------------------------------
 
-class ROIPlot():
-    ...
+class ROIPlot(DataArrayPlot):
+
+    def __init__(self, parent=None, data_array=None, plotItem=None) -> None:
+        super(ROIPlot, self).__init__(parent, plotItem)
 
 # ----------------------------------------------------------------------------------
